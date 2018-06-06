968d37afda6ad5e0c384d6240f31825c3ec3d669
Edwards <Peter.Edwards@esko.com>
2018-06-06 10:16:04 +0100

    Added Cat Level

---
+++ js/app.js
@@ -0,0 +1,23 @@
+var ViewModel = function() {
+    this.clickCount = ko.observable(0);
+    this.name = ko.observable('Tabby');
+    this.imgSrc = ko.observable('img/434164568_fea0ad4013_z.jpg');
+    this.imgAttribution = ko.observable('https//www.flickr.com/photos/big')
+    this.catLevel = ko.computed(function() {
+        var count = this.clickCount();
+        if(count < 10) {
+            return "Newborn";
+        }else if(count < 20){
+            return "Infant";
+        }else if(count < 100) {
+            return "Teen";
+        }else {
+            return "Adult";
+        }
+    }, this)
+    this.incrementCounter = function() {
+        this.clickCount(this.clickCount() + 1);
+    };
+}
+
+ko.applyBindings(new ViewModel());
